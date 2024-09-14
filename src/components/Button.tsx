8dce35735391be5aee5c3f7a9cfce648e2ab8ae6
Zameerahmad01 <zameerahmad4877@gmail.com>
2024-09-14 12:10:49 +0500

    travel app

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,12 +5,15 @@ type BUttonProps = {
   title: string;
   icon?: string;
   variant: string;
+  full?: boolean;
 };
 
-const Button = ({ type, title, icon, variant }: BUttonProps) => {
+const Button = ({ type, title, icon, variant, full }: BUttonProps) => {
   return (
     <button
-      className={`flexCenter gap-2  ${variant} rounded-3xl px-4 py-2 transition-all hover:opacity-80`}
+      className={`flexCenter gap-2  ${variant} ${
+        full && "w-full"
+      } rounded-3xl px-4 py-2 transition-all hover:opacity-80`}
       type={type}
     >
       {icon && <Image src={icon} alt={title} width={20} height={20} />}
